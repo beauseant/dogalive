16390f7cba653a539f00228b0beba6eb206185da
beauseant <suscripcion@bluethinking.com>
2013-11-30 23:17:38 +0100

    Version inicial, realiza un ping con hilos a un conjunto de maquinas.

---
+++ lib/threadping.py
@@ -0,0 +1,51 @@
+import Queue
+import threading
+import subprocess
+import logging
+
+
+
+class ThreadPing (threading.Thread):
+
+	__idth		= 0
+	__log		= ''
+	__queue		= Queue.Queue()
+	__resultado 	= {}
+
+	def __init__(self, queue, idth, log):
+		threading.Thread.__init__(self)
+
+		self.__idth 	= idth
+		self.__queue 	= queue
+		self.__log 	= log
+
+
+	def __ping ( self, host ):
+		return ( subprocess.call("ping -c 1 %s" % host, shell=True, stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT) )
+		
+
+	def run(self):
+		while True:
+			#Recogemos de la cola
+			host = self.__queue.get()
+			if ( self.__ping ( host ) == 0 ):
+				mensaje = 'el host %s estaba vivo' % ( host )
+				self.__resultado [ host ] = 1
+			else:
+				mensaje = 'el host %s NO estaba vivo' % ( host )
+				self.__resultado [ host ] = 0
+
+
+			if  self.__log:
+				logging.debug ( mensaje )
+
+			#marcamos tarea como finalizada:
+			self.__queue.task_done()
+
+
+	def getResultados ( self ):
+		return self.__resultado
+
+	def testing ( self ):
+		print 'testing'
+
---
+++ main.py
@@ -0,0 +1,58 @@
+import Queue
+import time
+import argparse
+import logging
+import lib.threadping
+
+
+def main( queue, log ):
+
+	num_hilos 	= 3
+	inicio		= 10
+	final		= 13
+	direc_base	= '192.168.1.'
+	
+	#generamos los hilos que queremos y en cada uno pasamos la cola como instancia y el identificador:
+	for i in range( num_hilos ):
+		t = lib.threadping.ThreadPing (queue, i, log )
+		t.setDaemon ( True )
+		t.start()
+
+	#Metemos en la cola lo que queremos procesar y que sera recogido por los hilos anteriores:
+	for host in range ( inicio , int (final) ) :
+		queue.put(direc_base + str (host) )
+
+	#Nos quedamos esperando a que no quede nada por procesar:
+	queue.join()
+
+	#Recogemos los datos de cada uno de los hilos:
+	Salida = {}
+	for i in range( num_hilos ):
+		Salida.update ( t.getResultados () )
+
+	for k,v in Salida.iteritems ():
+		print k
+		print v
+
+
+if __name__ == "__main__":
+
+
+	log = 0
+	parser    = argparse.ArgumentParser ( description= 'ping con hilos' )
+	parser.add_argument('--d', action="store_true", help='imprimir informacion de debug')
+
+	args     =    parser.parse_args()
+
+	if args.d:
+		logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+		log = 1
+
+
+	queue = Queue.Queue()
+
+	start = time.time()
+
+	main( queue, log )
+
+	print "Elapsed Time: %s" % (time.time() - start)
